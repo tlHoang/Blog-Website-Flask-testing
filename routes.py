d06b1d6e1556077c3249a4b10b8d62be8a6ec158
Phat <tanphat181203@gmail.com>
2024-04-25 14:13:49 +0700

    Login

--- routes.py
+++ routes.py
@@ -1,8 +1,10 @@
-from flask import render_template, request
-
-from models import User, create_user
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+from models import User, create_user, checkLogin
 from app import app
 
+app.permanent_session_lifetime = timedelta(minutes=5)
+app.config['SECRET_KEY'] = 'tanphat'
 
 @app.route('/')
 def index():
@@ -24,3 +26,20 @@ def add():
 
     user = create_user(username, email, password)
     return render_template('add.html', user=user)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.permanent = True
+        username = request.form.get('username_field')
+        password = request.form.get('password_field')
+        checkUser = checkLogin(username, password)
+        if checkUser:
+            session['user'] = checkUser
+            return redirect(url_for('user'))
+    return render_template('login.html')
+
+@app.route('/user')
+def user():
+    if 'user' in session:
+        return session['user']
