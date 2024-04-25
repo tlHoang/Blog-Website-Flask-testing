29de162efee676dd29f130a4c9177a8fed49c7af
Huy Hoang <tlhh232003@gmail.com>
2024-04-25 14:15:10 +0700

    test

--- routes.py
+++ routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, request
+from flask import render_template, request, session, redirect, url_for, render_template_string
 
 from models import User, create_user
 from app import app
 
+app.config['SECRET_KEY'] = '1111'
 
 @app.route('/')
 def index():
@@ -24,3 +25,17 @@ def add():
 
     user = create_user(username, email, password)
     return render_template('add.html', user=user)
+
+@app.get('/logout')
+def logout():
+    session.pop('user', default=None)
+    return redirect(url_for('login'))
+
+@app.get('/post')
+def getpostpage():
+    session['user'] = 'user1'
+    return render_template('post.html')
+
+@app.post('/post')
+def post_bai():
+    if 'user' in session:
