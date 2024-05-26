69dd626c290d271859b439a2fad753d93c371d42
tlHoang <tlhh232003@gmail.com>
2024-05-26 09:52:45 +0700

    delete migrate

--- app.py
+++ app.py
@@ -1,34 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-from logging.config import dictConfig
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
-
-# Import database models with app context
-with app.app_context():
-    from models import *
-migrate = Migrate(app, db)
 
 if __name__ == "__main__":
 
