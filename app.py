9bd7c835b1dccf9d1f997c83c55c2cf7f4cbbda0
HHoang <89715879+tlHoang@users.noreply.github.com>
2024-05-26 14:10:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
 
+from routes import *
+
 if __name__ == "__main__":
 
-    from routes import *
-    app.logger.info("Starting app")
     app.run(debug=True)
